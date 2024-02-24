7077fecd90675f82a489beff079fd455f07be2ea
Benoît <benoitsagna7@gmail.com>
2024-02-24 10:07:34 +0000

    check_up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
-import Navbar  from './navbar.jsx';
-import reactLogo from './assets/react.svg';
+import reactLogo from './assets/gm.svg';
 import viteLogo from '/vite.svg';
-import 'tailwindcss/tailwind.css'; //import du tailwind css
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import Navbar  from './navbar.jsx';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1 className="text-3x1 font-semibold text-center mt-4">Google Maps Example </h1>
+      <Map/>
+    </div>
+  );
+};
 
 const containerStyle = {
   width: '400px',
@@ -16,29 +26,34 @@ const center = {
   lng: -38.523,
 };
 
+const markers = [
+  { position: { lat: -3.745, lng: -38.523 }, title: 'Marqueur 1' },
+  { position: { lat: -3.755, lng: -38.523 }, title: 'Marqueur 2' },
+  { position: { lat: -3.765, lng: -38.523 }, title: 'Marqueur 3' },
+];
+
+//Logique de la carte Google Maps 
 function Map() {
   const { isLoaded } = useJsApiLoader ({
     id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyAxkC-LZ4ZVaTVxCok278D-D6X-RiJbF1o',
+    googleMapsApiKey: 'AIzaSyA4_2dsgP4a6pejNop_Zho88S3xdVRz5BY',
   });
 
-  return isLoaded ? (
+  return isLoaded ? ( //contenu de la carte google maps
     <GoogleMap
       mapContainerStyle = {containerStyle}
       center={center}
       zoom={10}
-      ></GoogleMap>
+      >
+        {markers.map(marker => (
+          <Marker
+          key={marker.title}
+          position={marker.position}
+          title={marker.title}
+          />
+        ))}
+      </GoogleMap>
   ) : <></>;
-}
-
-function App() {
-  return (
-    <div>
-      <Navbar/>
-      <h1 className="text-3x1 font-semibold text-center mt-4">Google Maps Example </h1>
-      <Map/>
-    </div>
-  );
 };
 
 export default App;
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto">
-                <h1 className="text-white text-xl font-semibold"> Mon application Google Maps</h1>
+                <h1 className="text-black text-xl font-semibold"> Mon application Google Maps</h1>
             </div>
         </nav>
     );
